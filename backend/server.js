1c6a3154144eff595de9cd231349a7f3f07bd204
RustH3d <humbertoa00327@gmail.com>
2025-06-09 15:44:26 -0400

    gfgfgffgfgggggfgggfgf

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express= require('express')
 const app= express()
 const cors= require('cors')
+require('dotenv').config();
 const usersRoutes= require('./routes/users')
 const recipesRoutes= require('./routes/recipes')
 const groupsRoutes= require('./routes/groups')
-const PORT= process.env.PORT || 3000
+//const PORT= process.env.PORT || 3000
+const {Pool}= require({pg})
 
 app.use(cors())
 app.use(express.json())
@@ -17,6 +19,21 @@ app.get('/',(req,res)=>{
     res.send('Backend funcionando')
 })
 
-app.listen(PORT,()=>{
-    console.log(`Corriendo en el puerto http://localhost:${PORT}`)
+const pool= new pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV==='production'
 })
+
+/* const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "recetas",
+  password: "bionicle2006",
+  port: 5432,
+}); */
+
+module.exports= pool
+
+/* app.listen(PORT,()=>{
+    console.log(`Corriendo en el puerto http://localhost:${PORT}`)
+}) */
