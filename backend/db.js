3bc471238bf0e93faaa310f4448f1105b758a8da
RustH3d <humbertoa00327@gmail.com>
2025-06-12 14:13:11 -0400

    asasass

--- backend/db.js
+++ backend/db.js
@@ -2,13 +2,14 @@ const pg = require('pg');
 const { Pool } = require("pg");
 
 
-/* const pool = new Pool({
+ const pool = new Pool({
   user: "postgres",
   host: "localhost",
   database: "recetas",
-  password: "bionicle2006",
+  //password: "bionicle2006",
+  password: "L1nk3d",
   port: 5432,
-}); */
+}); 
 
 /* const pool= new pool({
     connectionString: process.env.DATABASE_URL,
@@ -18,13 +19,13 @@ const { Pool } = require("pg");
 
 
 
-const pool = new Pool({
+/* const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false, // requerido para Supabase
   },
 });
-
+ */
 
 
 module.exports = pool;
